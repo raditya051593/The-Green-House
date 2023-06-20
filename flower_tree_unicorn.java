e3cc97f90e0592742b948acf3c4f565fe7fb75a3
raditya051593 <136757262+raditya051593@users.noreply.github.com>
2023-06-20 22:35:40 +0700

    Fix broken tests

---
+++ flower_tree_unicorn.java
@@ -0,0 +1,226 @@
+public class GreenHouse {
+	
+	//Instance Variables
+    private String color;
+    private int numberOfDoors;
+    private int numberOfWindows;
+    private boolean hasGarden;
+    
+    //Constructors
+    public GreenHouse() {
+        
+    }
+    
+    public GreenHouse(String color, int numberOfDoors, int numberOfWindows, boolean hasGarden) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfWindows = numberOfWindows;
+        this.hasGarden = hasGarden;
+    }
+    
+    //Methods
+    public String getColor() {
+        return color;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+    
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+    
+    public int getNumberOfWindows() {
+        return numberOfWindows;
+    }
+    
+    public void setNumberOfWindows(int numberOfWindows) {
+        this.numberOfWindows = numberOfWindows;
+    }
+    
+    public boolean isHasGarden() {
+        return hasGarden;
+    }
+    
+    public void setHasGarden(boolean hasGarden) {
+        this.hasGarden = hasGarden;
+    }
+    
+    public void openDoor(){
+        System.out.println("Opening door");
+    }
+    
+    public void openWindow(){
+        System.out.println("Opening window");
+    }
+    
+    public void plantGarden(){
+        System.out.println("Planting garden");
+    }
+    
+    public void mowLawn(){
+        System.out.println("Mowing lawn");
+    }
+    
+    public void waterGarden(){
+        System.out.println("Watering garden");
+    }
+    
+    public void paintHouse(){
+        System.out.println("Painting house");
+    }
+    
+    public void cleanHouse(){
+        System.out.println("Cleaning house");
+    }
+    
+    public void repairRoof(){
+        System.out.println("Repairing roof");
+    }
+    
+    public void cleanWindows(){
+        System.out.println("Cleaning windows");
+    }
+    
+    public void installFence(){
+        System.out.println("Installing fence");
+    }
+    
+    public void fertilizeLawn(){
+        System.out.println("Fertilizing lawn");
+    }
+    
+    public void trimTrees(){
+        System.out.println("Trimming trees");
+    }
+    
+    public void installIrrigation(){
+        System.out.println("Installing irrigation system");
+    }
+    
+    public void trimBushes(){
+        System.out.println("Trimming bushes");
+    }
+    
+    public void pressureWash(){
+        System.out.println("Pressure washing house");
+    }
+    
+    public void addPorch(){
+        System.out.println("Adding porch");
+    }
+    
+    public void updateWindows(){
+        System.out.println("Updating windows");
+    }
+    
+    public void buildDeck(){
+        System.out.println("Building deck");
+    }
+    
+    public void addPatio(){
+        System.out.println("Adding patio");
+    }
+    
+    public void installShed(){
+        System.out.println("Installing shed");
+    }
+    
+    public void installLighting(){
+        System.out.println("Installing outdoor lighting");
+    }
+    
+    public void buildRetainingWall(){
+        System.out.println("Building retaining wall");
+    }
+    
+    public void installGutters(){
+        System.out.println("Installing gutters");
+    }
+    
+    public void addDriveway(){
+        System.out.println("Adding driveway");
+    }
+    
+    public void addWalkway(){
+        System.out.println("Adding walkway");
+    }
+    
+    public void constructFirePit(){
+        System.out.println("Constructing fire pit");
+    }
+    
+    public void installSprinklers(){
+        System.out.println("Installing sprinklers");
+    }
+    
+    public void replantGarden(){
+        System.out.println("Replanting garden");
+    }
+    
+    public void buildPathway(){
+        System.out.println("Building pathway");
+    }
+    
+    public void paintFence(){
+        System.out.println("Painting fence");
+    }
+    
+    public void replaceDoors(){
+        System.out.println("Replacing doors");
+    }
+    
+    public void cleanGutters(){
+        System.out.println("Cleaning gutters");
+    }
+    
+    public void soddingLawn(){
+        System.out.println("Sodding lawn");
+    }
+    
+    public void sealDriveway(){
+        System.out.println("Sealing driveway");
+    }
+    
+    public void reroofHouse(){
+        System.out.println("Reroofing house");
+    }
+    
+    public void addGardenLighting(){
+        System.out.println("Adding outdoor garden lighting");
+    }
+    
+    public void resealWindows(){
+        System.out.println("Resealing windows");
+    }
+    
+    public void pruneBushes(){
+        System.out.println("Pruning bushes");
+    }
+    
+    public void addAwning(){
+        System.out.println("Adding awnings");
+    }
+    
+    public void installPool(){
+        System.out.println("Installing pool");
+    }
+    
+    public void buildArbor(){
+        System.out.println("Building arbor");
+    }
+    
+    public void installFountain(){
+        System.out.println("Installing fountain");
+    }
+    
+    public void paintGarage(){
+        System.out.println("Painting garage");
+    }
+
+}
