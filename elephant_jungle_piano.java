9c596c719d224bf8ec052768fb573bdbd27f0ca5
raditya051593 <136757262+raditya051593@users.noreply.github.com>
2023-07-20 21:00:13 +0700

    Refactor code for scalability

---
+++ elephant_jungle_piano.java
@@ -0,0 +1,339 @@
+import java.util.*;
+
+public class GreenHouse {
+	
+	private String address;
+	private int area;
+	private int capacity;
+	
+	//Constructor
+	public GreenHouse(String address, int area, int capacity){
+		this.address = address;
+		this.area = area;
+		this.capacity = capacity;
+	}
+	
+	//getters
+	public String getAddress(){
+		return this.address;
+	}
+	
+	public int getArea(){
+		return this.area;
+	}
+	
+	public int getCapacity(){
+		return this.capacity;
+	}
+	
+	//setters
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+	public void setArea(int area){
+		this.area = area;
+	}
+	
+	public void setCapacity(int capacity){
+		this.capacity = capacity;
+	}
+	
+	//other methods
+	public void makeGarden(){
+		System.out.println("Making a beautiful garden in the Green House...");
+	}
+	
+	public void plantFlowers(){
+		System.out.println("Planting some colorful flowers in the Green House...");
+	}
+	
+	public void harvestFruits(){
+		System.out.println("Harvesting some juicy fruits in the Green House...");	
+	}
+	
+	public void trimBushes(){
+		System.out.println("Trimming the bushes in the Green House...");
+	}
+	
+	public void mowLawn(){
+		System.out.println("Mowing the lawn in the Green House...");
+	}
+	
+	public void waterPlants(){
+		System.out.println("Watering the plants in the Green House...");
+	}
+	
+	public void makeCompost(){
+		System.out.println("Making a compost pile in the Green House...");
+	}
+	
+	public void cleanHouse(){
+		System.out.println("Cleaning the Green House...");
+	}
+	
+	public void inspectGreenHouse(){
+		System.out.println("Inspecting the Green House...");
+	}
+	
+	public void pruneTrees(){
+		System.out.println("Pruning the trees in the Green House...");
+	}
+	
+	public void replenishSoil(){
+		System.out.println("Replenishing the soil in the Green House...");
+	}
+	
+	public void plantSeeds(){
+		System.out.println("Planting some seeds in the Green House...");
+	}
+	
+	public void paintHouse(){
+		System.out.println("Painting the Green House...");
+	}
+	
+	public void collectRain(){
+		System.out.println("Collecting rain water in the Green House...");
+	}
+	
+	public void buySupplies(){
+		System.out.println("Buying supplies for the Green House...");
+	}
+	
+	public void updateGreenHouse(){
+		System.out.println("Updating the Green House...");
+	}
+	
+	public void fixWindows(){
+		System.out.println("Fixing the windows in the Green House...");
+	}
+	
+	public void buildFurniture(){
+		System.out.println("Building some furniture for the Green House...");
+	}
+	
+	public void organizeGreenHouse(){
+		System.out.println("Organizing the Green House...");
+	}
+	
+	public void planGarden(){
+		System.out.println("Planning the garden in the Green House...");
+	}
+	
+	public void feedAnimals(){
+		System.out.println("Feeding the animals in the Green House...");
+	}
+	
+	public void takeCarePlants(){
+		System.out.println("Taking care of the plants in the Green House...");
+	}
+	
+	public void checkWaterLevel(){
+		System.out.println("Checking the water level in the Green House...");
+	}
+	
+	public void cleanFurniture(){
+		System.out.println("Cleaning the furniture in the Green House...");
+	}
+	
+	public void buyPlants(){
+		System.out.println("Buying some plants for the Green House...");
+	}
+	
+	public void checkTemperature(){
+		System.out.println("Checking the temperature in the Green House...");
+	}
+	
+	public void applyFertilizer(){
+		System.out.println("Applying fertilizer in the Green House...");
+	}
+	
+	public void weedGarden(){
+		System.out.println("Weeding the garden in the Green House...");
+	}
+	
+	public void rakeGarden(){
+		System.out.println("Raking the garden in the Green House...");
+	}
+	
+	public void shovelGarden(){
+		System.out.println("Shoveling the garden in the Green House...");
+	}
+	
+	public void checkSoilHealth(){
+		System.out.println("Checking the soil health in the Green House...");
+	}
+	
+	public void controlInsects(){
+		System.out.println("Controlling the insects in the Green House...");
+	}
+	
+	public void monitorHumidity(){
+		System.out.println("Monitoring the humidity in the Green House...");
+	}
+	
+	public void monitorLight(){
+		System.out.println("Monitoring the light in the Green House...");
+	}
+	
+	public void propagatePlants(){
+		System.out.println("Propagating the plants in the Green House...");
+	}
+	
+	public void treatDiseases(){
+		System.out.println("Treating the diseases in the Green House...");
+	}
+	
+	public void removeWeeds(){
+		System.out.println("Removing the weeds in the Green House...");
+	}
+	
+	public void cleanTools(){
+		System.out.println("Cleaning the tools in the Green House...");
+	}
+	
+	public void checkIrrigation(){
+		System.out.println("Checking the irrigation in the Green House...");
+	}
+	
+	public void addFertilizers(){
+		System.out.println("Adding fertilizers in the Green House...");
+	}
+	
+	public void inspectPests(){
+		System.out.println("Inspecting the pests in the Green House...");
+	}
+	
+	public void repairFences(){
+		System.out.println("Repairing the fences in the Green House...");
+	}
+	
+	public void layMulch(){
+		System.out.println("Laying the mulch in the Green House...");
+	}
+	
+	public void checkAirQuality(){
+		System.out.println("Checking the air quality in the Green House...");
+	}
+	
+	public void applyPesticides(){
+		System.out.println("Applying the pesticides in the Green House...");
+	}
+	
+	public void aerateSoil(){
+		System.out.println("Aerating the soil in the Green House...");
+	}
+	
+	public void changePlants(){
+		System.out.println("Changing the plants in the Green House...");
+	}
+	
+	public void adjustVentilation(){
+		System.out.println("Adjusting the ventilation in the Green House...");
+	}
+	
+	public void checkPlantHealth(){
+		System.out.println("Checking the plant health in the Green House...");
+	}
+	
+	public void installLighting(){
+		System.out.println("Installing the lighting in the Green House...");
+	}
+	
+	public void testSoil(){
+		System.out.println("Testing the soil in the Green House...");
+	}
+	
+	public void checkTemperatureHumidity(){
+		System.out.println("Checking the temperature and humidity in the Green House...");
+	}
+	
+	public void changeSoil(){
+		System.out.println("Changing the soil in the Green House...");
+	}
+	
+	public void prepareGreenHouse(){
+		System.out.println("Preparing the Green House...");
+	}
+	
+	public void checkMoistureLevel(){
+		System.out.println("Checking the moisture level in the Green House...");
+	}
+	
+	public void monitorPlantGrowth(){
+		System.out.println("Monitoring the plant growth in the Green House...");
+	}
+	
+	public void setIrrigationSystem(){
+		System.out.println("Setting up an irrigation system in the Green House...");
+	}
+	
+	public void disinfectGreenHouse(){
+		System.out.println("Disinfecting the Green House...");
+	}
+	
+	public void checkpHLevel(){
+		System.out.println("Checking the pH level in the Green House...");
+	}
+	
+	public void checkAirFlow(){
+		System.out.println("Checking the air flow in the Green House...");
+	}
+	
+	public void installPump(){
+		System.out.println("Installing a water pump in the Green House...");
+	}
+	
+	public void fertilizePlants(){
+		System.out.println("Fertilizing the plants in the Green House...");
+	}
+	
+	public void installHydroponicSystem(){
+		System.out.println("Installing a hydroponic system in the Green House...");
+	}
+	
+	public void mixSoil(){
+		System.out.println("Mixing the soil in the Green House...");
+	}
+	
+	public void buildGreenHouse(){
+		System.out.println("Building the Green House...");
+	}
+	
+	public void checkLeaks(){
+		System.out.println("Checking for any leaks in the Green House...");
+	}
+	
+	public void cleanEquipment(){
+		System.out.println("Cleaning the equipment in the Green House...");
+	}
+	
+	public void plantTrees(){
+		System.out.println("Planting some trees in the Green House...");
+	}
+	
+	public void monitorAnimals(){
+		System.out.println("Monitoring the animals in the Green House...");
+	}
+	
+	public void checkVentilation(){
+		System.out.println("Checking the ventilation in the Green House...");
+	}
+	
+	public void installGreenhouse(){
+		System.out.println("Installing the Green House...");
+	}
+	
+	public void adjustLight(){
+		System.out.println("Adjusting the light in the Green House...");
+	}
+	
+	public void trimTrees(){
+		System.out.println("Trimming the trees in the Green House...");
+	}
+	
+	public void rearrangeGarden(){
+		System.out.println("Rearranging the garden in the Green House...");
+	}
+
+}
